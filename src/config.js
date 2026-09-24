9ab1a1e7e9a7ed788fe32b3b91389c852499963d
SalvaInf <salvas.inf@gmail.com>
2026-09-24 18:12:11 -0500

    fail attempt 2

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// Credencial escrita a proposito para demostrar la compuerta de secretos (Lab 3).
+const awsConfig = {
+  region: "us-east-1",
+  accessKeyId: "AKIA3H5GZWJTSNEKVKJE",
+  secretAccessKey: "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",
+};
+
+module.exports = awsConfig;
